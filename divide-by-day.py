4c33678494db782ecfdc6130ca2aca3c14374ae6
Joan Massachs <joan.massachs@eurecat.org>
2023-03-27 11:39:44 +0200

    indenta amb 4 espais

--- divide-by-day.py
+++ divide-by-day.py
@@ -12,29 +12,29 @@ from twarc import ensure_flattened
 @click.argument('infile')
 @click.argument('outdir')
 def main(count_lines, lines, granularity, infile, outdir):
-  indices = {
-    'year': 4,
-    'month': 7,
-    'day': 10,
-    'hour': 13,
-    'minute': 16,
-    'second': 19
-  }
-  index = indices[granularity]
+    indices = {
+        'year': 4,
+        'month': 7,
+        'day': 10,
+        'hour': 13,
+        'minute': 16,
+        'second': 19
+    }
+    index = indices[granularity]
 
-  if count_lines:
-    with open(infile) as f:
-      lines = sum(1 for line in f)
-  elif lines is not None:
-    lines = int(lines)
+    if count_lines:
+        with open(infile) as f:
+            lines = sum(1 for line in f)
+    elif lines is not None:
+        lines = int(lines)
 
-  with open(infile) as infile:
-    for line in tqdm(infile, total=lines):
-      for t in ensure_flattened(json.loads(line)):
-        date = t['created_at'][:index]
-        with open(outdir + date + '.jsonl', 'a') as out:
-          out.write(json.dumps(t) + '\n')
+    with open(infile) as infile:
+        for line in tqdm(infile, total=lines):
+            for t in ensure_flattened(json.loads(line)):
+                date = t['created_at'][:index]
+                with open(outdir + date + '.jsonl', 'a') as out:
+                    out.write(json.dumps(t) + '\n')
 
 
 if __name__ == '__main__':
-  main()
+    main()
