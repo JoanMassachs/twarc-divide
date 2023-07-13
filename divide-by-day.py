219fe2ffc65d905fd72e9bfbd272366ead6b1d8c
Joan Massachs <joan.massachs@eurecat.org>
2023-07-13 09:42:21 +0200

    more documentation

--- divide-by-day.py
+++ divide-by-day.py
@@ -7,10 +7,26 @@ from twarc import ensure_flattened
 
 
 @click.command()
-@click.option('--granularity', '-g', default='day', show_default=True)
-@click.argument('infile', type=click.File('r'))
+@click.option(
+    '--granularity',
+    '-g',
+    type=click.Choice(
+        ['year', 'month', 'day', 'hour', 'minute', 'second'],
+        case_sensitive=False
+    ),
+    default='day',
+    show_default=True,
+    help='Granularity of temporal windows'
+)
+@click.argument('infile', type=click.File('r'), default='-')
 @click.argument('outdir', type=click.Path(exists=False))
 def main(granularity, infile, outdir):
+    '''
+    Divides the input file in an output file for each temporal window.
+    Each output file contains the tweets published in its temporal window.
+    Output files are saved in the output directory,
+    and their names are the dates of the temporal windows.
+    '''
     indices = {
         'year': 4,
         'month': 7,
